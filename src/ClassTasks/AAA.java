41910ca0d371e56b16757aec55180cd3acab33a7
Kanan Musayev <kanan.musayev@lesroches.ch>
2020-07-27 18:45:56 -0400

    update

---
+++ src/ClassTasks/AAA.java
@@ -0,0 +1,22 @@
+package ClassTasks;
+
+public interface AAA {
+
+
+    public static void main(String[] args) {
+
+    }
+    static void ali(){
+
+        System.out.println("Salam");
+
+    }
+
+
+
+
+}
+
+class a{
+    fjadhfjds..
+}
---
+++ src/ClassTasks/bbb.java
@@ -0,0 +1,4 @@
+package ClassTasks;
+
+public class bbb {
+}
